51ba183cbff72de842fbb5d29bac181b4595ab39
KMSkelton <kmskelton@outlook.com>
2015-08-03 21:34:45 -0700

    JS only does logic for looping through numbers and creates an array (no modulo logic). Had to add a class to the numbers we don't want to show. Without class, CSS would not show numbers but would show appropriate 'fizz' or 'buzz' in their place. With class='hide' those are now set to display='none'.

--- fizzbuzz2.js
+++ fizzbuzz2.js
@@ -1,29 +1,18 @@
-// if div by 3 print fizz
-// if div by 5 print buzz
-// if div by 3 and 5 print fizzbuzz
 function fizzbuzz() {
   var start = document.getElementById("start").value;
   start = parseInt(start);
   var finish = document.getElementById("finish").value;
   finish = parseInt(finish);
   var fizzbuzz = [];
-  console.log("start fizz buzz with:");
-  console.log(typeof(start));
-  console.log(finish);
+  for (var i = 1; i < start; i++) {
+    console.log("0 to start " + i)
+    fizzbuzz.push("<li class='hide'></li>");
+  }
   for (var i = start; i <= finish; i++) {
-    console.log(i);
-    if (i % 3 === 0 && i % 5 === 0) {
-      fizzbuzz += "<li class='fizz buzz'>fizzbuzz</li>";
-    } else if (i % 5 === 0) {
-      fizzbuzz += "<li class='buzz'>buzz</li>";
-    } else if (i % 3 === 0) {
-      fizzbuzz += "<li class='fizz'>fizz</li>";
-    } else {
-      console.log(i);
-      fizzbuzz += "<li>" + i + "</li>";
-    }
-  };
-  console.log("loop complete");
-  document.getElementById("result").innerHTML = fizzbuzz;
+    console.log("start to finish " + i);
+    fizzbuzz.push("<li><div>" + i + "</div></li>");
+  }
+  console.log("done " + i);
+  document.getElementById("result").innerHTML = fizzbuzz.join("");
+  console.log(fizzbuzz.join(""));
 };
-// got it on 3rd try. First time forgot to use ===, second time had to make i <= 100 as it was only running to 99
